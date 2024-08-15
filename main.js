4296ec5fe79df700415dccb82154ad3457121e20
Galtbatzana <galt.batzana1@gmail.com>
2024-08-15 20:12:23 +0800

    delete edit backend

--- main.js
+++ main.js
@@ -2,9 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
 
-
-
-
 const app = express()
 const port = 4000
 
@@ -38,7 +35,7 @@ app.listen(port, () => {
 // RAM, FILE, REMOTE
 
 //categories dotor orj irsen string-iig zuw JSON bolgon hurwuulne.
-const categories = JSON.parse(content)
+let categories = JSON.parse(content)
 
 // ene-iig unshuulahiin tuld 
 
@@ -55,39 +52,38 @@ app.get("/categories/list", (req, res)=> {
 
 
 ////////////////////////////////////////////////////
-// herev hussen zuilee bichsen zuilee gargii gevel ingej bichne
+// herev hussen zuilee, bichsen zuilee gargii gevel ingej bichne
 // data-gaa uuruu zohiogood tsaanaa hadgalj chadaj bn
-
+// !!! herev dahin davtagdashgui id: tai baiwal delete/edit hiih bolomjtoi bolno!!!!
 app.get("/categories/create", (req, res)=> {
   const { name } = req.query;
-  categories.push({ name: name });
-
+  categories.push ({ 
+    id: new Date().toISOString(),
+    name: name });
   fs.writeFileSync("categories.json", JSON.stringify(categories)) //data.json dotor 
-
   res.json(["Success"]);
 ;})
 
 // za eniigee front-end-teigee holboyo
 
-
-
-
 ////////////////////////////////////////
 // UPDATE HIIH !!!
 
 app.get("/categories/update", (req, res)=> {
-  // categories.push({ name: "New Categories" });
-  // TODO 
+  const {id, name} = req.query;
+  const index = categories.findIndex((cat)=>(cat.id===id));
+  categories[index].name = name;
+  fs.writeFileSync("categories.json", JSON.stringify(categories))
   res.json(["Success"]);
 ;})
-
 
 ///////////////////////////////////////////
 // DELETE HIIH !!!
 
 app.get("/categories/delete", (req, res)=> {
-  // categories.push({ name: "New Categories" });
-  // TODO 
+  const { id } = req.query;
+  categories = categories.filter((cat)=>(cat.id !== id));
+  fs.writeFileSync("categories.json", JSON.stringify(categories))
   res.json(["Success"]);
 ;})
 
