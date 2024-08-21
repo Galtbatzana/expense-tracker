424b19a9ed8fdb16982f5031a0bff52dea41522a
Galtbatzana <galt.batzana1@gmail.com>
2024-08-21 17:26:17 +0800

    database

--- configs/basic.js
+++ configs/basic.js
@@ -9,17 +9,13 @@ app.use(express.json());  //backend-ees body awna//
 
 
 
-
-
-function startApp() {
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
-  return app;
-}
 
 
 module.exports = {
-  startApp,
-  
+  app,
 }
+
+
--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 
-const { startApp } = require('./configs/basic');
-const { getCategories, createNewCategory, getOneCategory, updateCategory, deleteCategory } = require('./services/categoryService');
+// const { startApp } = require('./configs/basic');
+// const { getCategories, createNewCategory, getOneCategory, updateCategory, deleteCategory } = require('./services/categoryService');
 
-const app = startApp();
+// const app = startApp();
+
+
+const { app } = require("./configs/basic");
+const { sql } = require("./configs/database");
+
+const { getCategories, createCategory} = require('./services/categoryService');
+
 //////////////////////////////////////////////////////////
 // app.get('/', (req, res) => {
 //   res.send('Hello World!')
@@ -24,23 +31,18 @@ const app = startApp();
 
 
 ///////////////////////////////////////////////////////
-app.get("/categories", (req, res)=> {
-  const categories = getCategories();
-  res.json(categories);
-});
+// app.get("/categories", (req, res)=> {
+//   const categories = getCategories();
+//   res.json(categories);
+// });
 
 ////////////////////////////////////////////////////////
 // herev 1 shirhegiig awya gewel
-app.get("/categories/:id", (req, res)=> {
-  const { id } = req.params;
-  const one = getOneCategory(id);
-  // if(id !== cat.id) {
-  //   res.status(404).json({massege: "Wrong id, try again!"})
-  //   return;
-  // }
-  // const categories = categories.find((cat.id === id))
-  res.json(one);
-});
+// app.get("/categories/:id", (req, res)=> {
+//   const { id } = req.params;
+//   const one = getOneCategory(id);
+//   res.json(one);
+// });
  
 //////////////////////////////////////////////////
 
@@ -57,38 +59,62 @@ app.get("/categories/:id", (req, res)=> {
 
 // ene function ni 1 yum awlaa butsaalaa tegeed boloo ///////
 // createNewCategory function ni tsaashaa hadgaldag uildel hiij bga//
-app.post("/categories", async (req, res)=> {
-  const { name } = req.body;
-  const id = await createNewCategory({ name });
-  res.status(201).json({ id });
-});
+// app.post("/categories", async (req, res)=> {
+//   const { name } = req.body;
+//   const id = await createNewCategory({ name });
+//   res.status(201).json({ id });
+// });
 
 // za eniigee front-end-teigee holboyo
 
 ////////////////////////////////////////
 // UPDATE HIIH !!!
 
-app.put("/categories/:id", async (req, res)=> {
-  const { id } = req.params;
-  const { name } = req.body;
-  if (!name) {
-    res.status(404).json({message: "`Name` field is required!"});
-    return;
-  }
-  await updateCategory(id, name);
-  res.sendStatus(204);
-});
+// app.put("/categories/:id", async (req, res)=> {
+//   const { id } = req.params;
+//   const { name } = req.body;
+//   if (!name) {
+//     res.status(404).json({message: "`Name` field is required!"});
+//     return;
+//   }
+//   await updateCategory(id, name);
+//   res.sendStatus(204);
+// });
 
 ///////////////////////////////////////////
 // DELETE HIIH !!!
 
-app.delete("/categories/:id", async (req, res)=> {
-  const { id } = req.params;
-  await deleteCategory(id);
-  // if (deleteIndex < 0) {
-  //   res.sendStatus(404);
-  //   return;
-  // }
-  res.sendStatus(204);
-;})
+// app.delete("/categories/:id", async (req, res)=> {
+//   const { id } = req.params;
+//   await deleteCategory(id);
+//   // if (deleteIndex < 0) {
+//   //   res.sendStatus(404);
+//   //   return;
+//   // }
+//   res.sendStatus(204);
+// ;})
+
+//////////////////////////////////////////////////////////////////////
+/////////////////jinhen database-tei holbogdoh///////////////////////
+
+////////////GET/////////////////////////////////////////////////////
+app.get("/categories", async (req, res) => {
+  const list = await getCategories();
+  res.json(list);
+});
+
+///////////////////POST////////////////////////////
+app.post("/categories", async (req, res) => {
+  const input = req.body;
+  const id = await createCategory(input);
+  res.status(201).json({id});
+});
+
+///////////////TEST////////////////////////
+
+app.get("/dbtest", async (req, res) => {
+  const result = await sql`select version()`;
+  console.log(result);
+  res.json({ result });
+});
 
